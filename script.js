d25f24c2c1bf44d3ad223dc38c0783fa582ebe79
CodAst <avalenzuelaz@est.ups.edu.ec>
2025-06-09 16:43:41 -0500

    Variables

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function mostrarMensaje()
 {
     alert("Hola Mundo");
 }
+
+//Variable
+function imprimirVariable()
+{
+    let nombre = "Astrid";
+    let edad = "20"
+
+    //Forma tradicional de concatenar cadenas
+    console.log("nombre: ", nombre);
+    console.log("edad: ", edad);
+    
+    //Forma moderna de concatener cadenas
+    console.log[`nombre: ${nombre}`, `edad: ${edad}`];
+    //Otros tipos de impresiones
+    console.error("Error: bla bla bla");
+    console.warn("Advertencia: x x x");
+}
